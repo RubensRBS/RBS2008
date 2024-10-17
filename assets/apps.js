d8234139b1fb6b40e19bc0bab0aa8959b2980737
RubensRBS <145141420+RubensRBS@users.noreply.github.com>
2024-10-17 09:10:29 -0300

    kk

--- assets/apps.js
+++ assets/apps.js
@@ -5,3 +5,9 @@ function criarCartao(categoria, pergunta, resposta) {
 function criaCartao(categoria, pergunta, resposta) {
     console.log(categoria, pergunta, resposta);
     }
+    let respostaEstaVisivel = false
+    function viraCartao () {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toogle( 'active', respostaEstaVisivel)
+    }
+    cartao.addEventListener( 'click', viraCartao)
