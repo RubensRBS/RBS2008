6a629044b37debdfead90ee70e149c90467beda8
RubensRBS <145141420+RubensRBS@users.noreply.github.com>
2024-11-07 18:11:59 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+const perguntas = [
+    {
+        pergunta: "em que ano o roblox foi criado",
+        resposta: "2006",
+    },
+    {
+        pergunta: "o mais gay",
+        resposta: "neres."
+    },
+    {
+        pergunta: "0 x 0",
+        resposta: "0"
+    },
+    {
+        pergunta: "2+2",
+        resposta: "4."
+    }
+];
